69a61f3b06d0ae898e61c79d86992d63746f5d53
Cryphex <mods_vincas_2s@icloud.com>
2026-03-29 15:07:43 +0200

    Fixed major port issue for hosting on render

--- webserver.py
+++ webserver.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from threading import Thread
 from waitress import serve
-
+from os import environ
 app = Flask('')
 
 @app.route('/')
@@ -9,7 +9,8 @@ def index():
     return 'Discord Bot ok'
 
 def run():
-    serve(app, host='0.0.0.0', port=8080)
+    port = int(environ.get("PORT", 10000))
+    serve(app, host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
