a5262ea9f0a07139f1781dbafc47a25d5545f5cc
Cryphex <mods_vincas_2s@icloud.com>
2026-04-03 12:01:33 +0200

    Fix indentation in webserver.py

--- webserver.py
+++ webserver.py
@@ -7,7 +7,7 @@ def home():
     return 'Discord Bot ok'
 
 def run():
-    app.run(app, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
