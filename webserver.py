b883e7801e8ae94453b7d734dc239c72e80b81c0
Cryphex <271290898+cryphex@users.noreply.github.com>
2026-04-04 09:47:57 +0200

    Use environment variable for server port
    Updated webserver to use environment variable for port.

--- webserver.py
+++ webserver.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask
 from threading import Thread
 
 app = Flask('')
+
 @app.route('/')
 def home():
-    return 'Discord Bot ok'
+    return "Discord Bot ok"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
